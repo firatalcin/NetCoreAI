64861236e1ed1a95269d59be42530ab3980c8d5e
Fırat Alçın <firatalcin04@gmail.com>
2025-10-13 23:12:18 +0300

    RapidApi

---
+++ NetCoreAI/NetCoreAI.Project03_RapidApi/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetCoreAI.Project03_RapidApi.ViewModels;
+using Newtonsoft.Json;
+
+
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "630ce9cc86msh271c60cffe62d5ep1b514djsn0fe292593744" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach (var series in apiSeriesViewModels)
+    {
+        Console.WriteLine(series.rank + "-" + series.title + "-Film Puanı: " + series.rating + "-Yapım Yılı: " + series.year);
+    }
+}
+
+Console.ReadLine();
